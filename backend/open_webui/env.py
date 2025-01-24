aa3f6df1ecccf985b9cb6cf8f5755fa59d5197cc
Gustavo Santana Silva <gssilva@cervejariacidadeimperial.com.br>
2025-01-24 10:28:56 -0300

    atualizacao de imagem e texto v2

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -106,11 +106,11 @@ for source in log_sources:
 log.setLevel(SRC_LOG_LEVELS["CONFIG"])
 
 
-WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
-if WEBUI_NAME != "Open WebUI":
-    WEBUI_NAME += " (Open WebUI)"
+WEBUI_NAME = os.environ.get("WEBUI_NAME", "IA CCI")
+if WEBUI_NAME != "IA CCI":
+    WEBUI_NAME += " (IA CCI)"
 
-WEBUI_FAVICON_URL = "https://openwebui.com/favicon.png"
+WEBUI_FAVICON_URL = "https://developers.google.com/favicon.png"
 
 
 ####################################
